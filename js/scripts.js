b5982f0791ba13fbf1e17b64e0a1b87023b6327f
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-09 11:50:24 -0700

    ask for a number to count to and a number to count by. then it will display those numbers to the page with a space. also tracks for NaN and negative integers along with a few other error handlers.
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-event.preventDefault();
-})
+function initCount() {
+  let countToInput = parseInt(prompt("Enter a number:"));
+  let multiple = parseInt(prompt("Enter another number to count by:"))
+  countToFunction(countToInput, multiple);
+
+  function countToFunction(countToInput, multiple) {
+  if (countToInput === undefined || multiple === undefined) {
+    alert("Please enter a valid number value.");
+    initCount();
+  } else if (countToInput < multiple) {
+    alert("Number you are counting to must be larger than your multiple.");
+    initCount();
+  } else if (Math.sign(countToInput) === -1 || Math.sign(multiple === -1)) {
+    alert("Please enter a positive number!");
+    initCount();
+  } else if (isNaN(countToInput) || isNaN(multiple)) {
+    alert("Please enter a valid number!");
+    initCount();
+  } else {
+    for (i=0; i<countToInput+1; i+=multiple) {
+    document.write(" " + i);  
+  }}}
+}
+
+initCount();
